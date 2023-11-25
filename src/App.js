5db5f20406e2cf26b7769f0e02316ff8475318ea
Amine Halal <aminenohalal@gmail.com>
2023-11-25 17:04:01 +0100

    To Do List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks , setTasks] = useState([]);
+  const newTask = useRef();
+
+  const handleAddTask = () => {
+    const task = newTask.current.value ;
+    const currentDate = new Date();
+
+    const currentTime = currentDate.toLocaleTimeString();
+    const theTask = {
+      completed : false ,
+      time : currentTime ,
+      text : task
+    }
+    setTasks([...tasks , theTask]);
+    newTask.current.value = '';
+    console.log(tasks)
+  }
+
+  const handleTaskDone = (index) => {
+    const copyTask = [...tasks];
+    copyTask[index].completed = !copyTask[index].completed;
+    setTasks(copyTask);
+  }
+
+  const handleDeleteTask = (index) => {
+    const copyTask = [...tasks];
+    copyTask.splice(index , 1);
+    setTasks(copyTask);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-kanit text-white">
+      <div className='flex flex-col justify-center items-center p-5 m-1'>
+        <h1 className='text-6xl m-9 font-extrabold font-kanit'>To <span className='text-cyan-400'>Do</span> List</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <ul className='font-bold text-slate-200 p-4'>
+            {tasks.map(({completed ,time, text}, index) => {
+              return (
+                <div key={index} className='flex justify-between items-center m-1 my-2'>
+                  <li onClick={() => handleTaskDone(index)} className={`cursor-pointer list-none hover:list-disc hover:list-inside w-64 sm:w-80 md:w-96 break-words p-1 ${completed ? 'line-through' : ''}`}>{text}</li>
+                  <span className='p-2 text-xs text-cyan-400'>{time}</span>
+                  <span onClick={()=>handleDeleteTask(index)} className='text-cyan-400 p-1 border-2 border-cyan-400 rounded-lg hover:border-red-500 hover:text-red-500 duration-700 cursor-pointer'>Delete</span>
+                </div>
+              );
+            })}    
+          </ul>
+          <input type='text' ref={newTask} className='border-2 border-cyan-400 rounded-lg p-1 m-2 bg-transparent font-bold text-black focus:bg-slate-100' />
+          <button onClick={handleAddTask} className='hover:font-bold duration-500 text-xl'>Add <span className='text-cyan-400'>Task</span></button>
+        </div>
+      </div>
     </div>
   );
 }
